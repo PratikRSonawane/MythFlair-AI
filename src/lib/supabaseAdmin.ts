57ccdf63b9783f6d8d1c3bcec8494e5e9c474bac
PratikRSonawane <pratik.sonawane.dev@gmail.com>
2025-12-03 01:08:27 +0530

    Update supabaseAdmin.ts

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -4,5 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// SERVER-SIDE ONLY client (has full DB permissions, so never import this in React components)
+
 export const supabaseAdmin = createClient(supabaseUrl, serviceKey);
