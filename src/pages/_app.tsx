a5f7f27cc2ee101293ac1357e80a7e51f9871920
PratikRSonawane <pratik.sonawane.dev@gmail.com>
2025-12-03 14:13:41 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,33 +6,36 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* Favicons for all pages */}
-        <link rel="icon" href="/favicon.ico" />
+  {/* Standard favicon */}
+  <link rel="icon" href="/favicon.ico" />
 
-        {/* iPhone/iPad icon */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+  {/* PNG favicons */}
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
-        {/* Android icons */}
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href="/icon-512x512.png"
-        />
+  {/* Apple icon */}
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
-        {/* Browser theming */}
-        <meta name="theme-color" content="#000000" />
-      </Head>
+  {/* Android */}
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="192x192"
+    href="/android-chrome-192x192.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="512x512"
+    href="/android-chrome-512x512.png"
+  />
+
+  {/* Favicon manifest */}
+  <link rel="manifest" href="/site.webmanifest" />
+
+  <meta name="theme-color" content="#000000" />
+</Head>
+
 
       <Component {...pageProps} />
     </>
